90fa7602b8e98cb954119ad93eb6cccee94f9c04
Waradamayanti <128689223+Waradamayanti@users.noreply.github.com>
2023-05-06 14:02:17 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Wara</li>
+        <li>Nama Belakang :Damayanti</li>
+        <li>Nama Lengkap : Wara Damayanti</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="disc">
+        <li> Jatijajar depok</li>
+    </ul>
+    
+    <h4>Tempat,Tanggal Lahir</h4>
+    <ul type="disc">
+        <li>Februari, 27 Februari 2002</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="disc">
+        <li>Berenang</li>
+        <li>bulu tangkis</li>
+    </ul>
+</section>
